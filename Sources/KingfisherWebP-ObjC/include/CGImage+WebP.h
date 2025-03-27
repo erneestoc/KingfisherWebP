ca63f4452794306ff1f31654750179f69c8e5a15
Ernesto Cambuston <ernesto.cambuston@instacart.com>
2025-03-27 13:39:26 -0600

    update headers

--- Sources/KingfisherWebP-ObjC/include/CGImage+WebP.h
+++ Sources/KingfisherWebP-ObjC/include/CGImage+WebP.h
@@ -19,10 +19,22 @@ CFDataRef __nullable WebPDataCreateWithImage(CGImageRef image, bool isLossy, flo
 CG_EXTERN const CFStringRef kWebPAnimatedImageDuration;
 CG_EXTERN const CFStringRef kWebPAnimatedImageLoopCount;
 CG_EXTERN const CFStringRef kWebPAnimatedImageFrames; // CFArrayRef of CGImageRef
+CG_EXTERN const CFStringRef kWebPAnimatedImageFrameDurations; // CFArrayRef of CFNumberRef
 
 uint32_t WebPImageFrameCountGetFromData(CFDataRef webpData);
 CFDictionaryRef __nullable WebPAnimatedImageInfoCreateWithData(CFDataRef webpData);
 CFDataRef __nullable WebPDataCreateWithAnimatedImageInfo(CFDictionaryRef imageInfo, bool isLossy, float quality);
+
+// accumulative decoding
+typedef struct WebPImageDecoder *WebPDecoderRef;
+
+WebPDecoderRef __nullable WebPDecoderCreateWithData(CFDataRef webpData);
+void WebPDecoderDestroy(WebPDecoderRef decoder);
+
+uint32_t WebPDecoderGetFrameCount(WebPDecoderRef decoder);
+CFTimeInterval WebPDecoderGetDurationAtIndex(WebPDecoderRef decoder, int index);
+CGImageRef __nullable WebPDecoderCopyImageAtIndex(WebPDecoderRef decoder, int index);
+
 
 CF_ASSUME_NONNULL_END
 CF_IMPLICIT_BRIDGING_DISABLED
